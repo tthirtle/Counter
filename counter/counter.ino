8f08074dc5f60ba9b93bafdebd5b94c0f7d09e86
BuildTools <unconfigured@null.spigotmc.org>
2019-05-20 18:49:33 -0500

    Update counter.ino

--- counter/counter.ino
+++ counter/counter.ino
@@ -8,7 +8,7 @@
  
 const int counterSensorPIN = 3;
 const int counterSwitchPIN = 5;
-const int resetButtonPIN = 7;
+// const int resetButtonPIN = 7;
 
 /*
  * Global Varibles
@@ -17,27 +17,46 @@ const int resetButtonPIN = 7;
  int blocked = 0;
  int lastState = 0;
  int counting = 0;
- /*
-  * change sensor to have built in pull up resistor
-  */
-  pinMode(counterSensorPIN,INPUT_PULLUP);
+ 
+
+
+void reset()
+{
+      Serial.println(counter);
+      counter = 0;
+}
   
 void setup() {
+ /*
+  * change sensor to have built in pull up resistor
+  */  
+    pinMode(counterSensorPIN,INPUT_PULLUP);
+    // Change switch to have a built in pull up resistor
+    pinMode(counterSwitchPIN,INPUT_PULLUP);
   // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // is switch for 
-//read sensor value and place it into blocked varible
-blocked = digitalRead(counterSensorPIN);
-if (blocked == 1) //is sensor blocked
-  {
-    if (lastState == 0) //was there a change since the last state was read (sensor just got blocked)
+  // read the switch
+  counting = digitalRead(counterSwitchPIN);
+  // if switch is off reset counter to 0.
+  if (counting == 0)
     {
-      counter = counter + 1;
-      Serial
-    }
+      reset();
+    } 
+    //if switch is on then turn on the counter
+  if (counting == 1)
+  {
+    //read sensor value and place it into blocked varible
+    blocked = digitalRead(counterSensorPIN);
+    if (blocked == 1) //is sensor blocked
+      {
+      if (lastState == 0) //was there a change since the last state was read (sensor just got blocked)
+        {
+          counter = counter + 1;
+        }
+      }
   }
 }
