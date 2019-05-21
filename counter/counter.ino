a3a102d3f2cb2e9622e40be2e7c6d9499b913458
BuildTools <unconfigured@null.spigotmc.org>
2019-05-21 14:16:27 -0500

    Update counter.ino
    update for the 1Sheeld board

--- counter/counter.ino
+++ counter/counter.ino
@@ -7,7 +7,7 @@
  */
  
 const int counterSensorPIN = 3;
-const int counterSwitchPIN = 5;
+// const int counterSwitchPIN = 5;
 // const int resetButtonPIN = 7;
 
 /*
@@ -18,7 +18,12 @@ const int counterSwitchPIN = 5;
  int lastState = 0;
  int counting = 0;
  
-
+/*
+ * Activate 1 Sheeld Library
+ */
+ #include <OneSheeld.h>
+ #define CUSTOM_SETTINGS
+ #define INCLUDE_TERMINAL_SHIELD
 
 void reset()
 {
@@ -32,23 +37,24 @@ void setup() {
   */  
     pinMode(counterSensorPIN,INPUT_PULLUP);
     // Change switch to have a built in pull up resistor
-    pinMode(counterSwitchPIN,INPUT_PULLUP);
+   // pinMode(counterSwitchPIN,INPUT_PULLUP);
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  // Serial.begin(9600);
+  OneSheeld.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   // read the switch
-  counting = digitalRead(counterSwitchPIN);
+  // counting = digitalRead(counterSwitchPIN);
   // if switch is off reset counter to 0.
-  if (counting == 0)
-    {
-      reset();
-    } 
+  // if (counting == 0)
+    // {
+    //   reset();
+    // } 
     //if switch is on then turn on the counter
-  if (counting == 1)
-  {
+  // if (counting == 1)
+  // {
     //read sensor value and place it into blocked varible
     blocked = digitalRead(counterSensorPIN);
     if (blocked == 1) //is sensor blocked
@@ -56,7 +62,8 @@ void loop() {
       if (lastState == 0) //was there a change since the last state was read (sensor just got blocked)
         {
           counter = counter + 1;
+          Terminal.println(counter);
         }
       }
-  }
+  // }
 }
